1add769960177424246790e32b7871fb3646dc43
JosephRidge <josephridge18@gmail.com>
2025-12-04 15:57:42 +0300

    updateed with mpesa darja

--- productsApp/views.py
+++ productsApp/views.py
@@ -1,6 +1,37 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django_daraja.mpesa.core import MpesaClient
 
 # Create your views here.
 def home(request):
     context ={}
     return render(request,'productsApp/home.html' , context)
+
+
+def index(request):
+    # cl = MpesaClient()
+    # Use a Safaricom phone number that you have access to, for you to be able to view the prompt.
+    # phone_number = '0719873406'
+    # amount = 1
+    # account_reference = 'reference'
+    # transaction_desc = 'Description'
+    # callback_url = 'https://api.darajambili.com/express-payment'
+    # response = cl.stk_push(phone_number, amount, account_reference, transaction_desc, callback_url)
+    return HttpResponse(response)
+
+def mpesaPayment(request):
+    cl = MpesaClient()
+    accountReference = 'SMS'
+    transactionDesc = 'Purchase of camel milk.'
+    callbackUrl = 'https://api.darajambili.com/express-payment'
+
+    # get user phonenumber
+    # get amount to pay
+    if request.method == 'POST':
+        phoneNumber = request.POST.get('phoneNumber')
+        amount = int(float(request.POST.get('amount')))
+        response = cl.stk_push(phoneNumber, amount, accountReference, transactionDesc, callbackUrl)
+        context ={"response":response}
+        return render(request, 'productsApp/mpesa_payments.html', context)
+    else:
+        return render(request, 'productsApp/mpesa_payments.html')
--- projectAuth/settings.py
+++ projectAuth/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles', 
     'authApp',
-    'productsApp'
+    'productsApp',
+    'django_daraja',
 ]
 
 MIDDLEWARE = [
@@ -122,3 +123,46 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# ============================ M-PESA =====================================
+
+# The Mpesa environment to use
+# Possible values: sandbox, production
+
+MPESA_ENVIRONMENT = 'sandbox'
+
+# Credentials for the daraja app
+
+MPESA_CONSUMER_KEY = 'cXWlRS7iauF2kHzreIGEtMJ2KRIyXvn9itOMRxjbsf1oiF81'
+MPESA_CONSUMER_SECRET = 'An3qksY5YaBpbU1l2q5HqtKJniH4dv2N6eARGRykCvmKx8aRLAawUffrqbOZQ9SY'
+
+#Shortcode to use for transactions. For sandbox  use the Shortcode 1 provided on test credentials page
+
+MPESA_SHORTCODE = 'mpesa_shortcode'
+
+# Shortcode to use for Lipa na MPESA Online (MPESA Express) transactions
+# This is only used on sandbox, do not set this variable in production
+# For sandbox use the Lipa na MPESA Online Shorcode provided on test credentials page
+
+MPESA_EXPRESS_SHORTCODE = '174379'
+
+# Type of shortcode
+# Possible values:
+# - paybill (For Paybill)
+# - till_number (For Buy Goods Till Number)
+
+MPESA_SHORTCODE_TYPE = 'paybill'
+
+# Lipa na MPESA Online passkey
+# Sandbox passkey is available on test credentials page
+# Production passkey is sent via email once you go live
+
+MPESA_PASSKEY = 'bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919'
+
+# Username for initiator (to be used in B2C, B2B, AccountBalance and TransactionStatusQuery Transactions)
+
+MPESA_INITIATOR_USERNAME = 'initiator_username'
+
+# Plaintext password for initiator (to be used in B2C, B2B, AccountBalance and TransactionStatusQuery Transactions)
+
+MPESA_INITIATOR_SECURITY_CREDENTIAL = 'initiator_security_credential'
